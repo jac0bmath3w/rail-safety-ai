4be9b90871aa1059895b68d33390bd0bcbae6e85
Jacob Mathew <jac0bmath3w@users.noreply.github.com>
2026-04-06 22:51:26 -0500

    pass colleciton_name as argument to constructor

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,11 +1,11 @@
 import chromadb
 
 class RailVectorVault:
-    def __init__(self, embedder_instance, db_path="./vector_db"):
+    def __init__(self, embedder_instance, db_path="./vector_db", collection_name = "rail_safety"):
         # We pass the embedder IN. This is called 'Dependency Injection'.
         self.embedder = embedder_instance 
         self.client = chromadb.PersistentClient(path=db_path)
-        self.collection = self.client.get_or_create_collection(name="rail_safety")
+        self.collection = self.client.get_or_create_collection(name=collection_name)
 
     def add_documents(self, chunks, metadatas):
         # The Vault asks the Embedder to do its job
